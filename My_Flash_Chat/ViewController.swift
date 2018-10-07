f5062bc53702ba20ec464c4884b878478cbf51b8
Bogdan Ponocko <bogdan-private@outlook.com>
2018-10-07 18:25:18 +0200

    Initial Commit

---
+++ My_Flash_Chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My_Flash_Chat
+//
+//  Created by Bogdan Ponocko on 10/7/18.
+//  Copyright © 2018 Bogdan Ponocko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
